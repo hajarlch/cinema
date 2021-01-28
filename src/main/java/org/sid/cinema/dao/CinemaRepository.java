b29685e84cea8aa2b427573bbf3e25b045cb34d8
hajarlch <hajarlahrech270@gmail.com>
2021-01-28 23:19:45 +0000

    envoi de mon projet

---
+++ src/main/java/org/sid/cinema/dao/CinemaRepository.java
@@ -0,0 +1,10 @@
+package org.sid.cinema.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.sid.cinema.entities.*;
+@RepositoryRestResource
+public interface CinemaRepository extends JpaRepository<Cinema,Long>{
+	
+	
+}
---
+++ src/main/java/org/sid/cinema/dao/SeanceRepository.java
@@ -0,0 +1,10 @@
+package org.sid.cinema.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.sid.cinema.entities.*;
+@RepositoryRestResource
+public interface SeanceRepository extends JpaRepository<Seance,Long>{
+	
+	
+}
